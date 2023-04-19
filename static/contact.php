b9b391bec7fed11883f384b27f30cc7fdf89af9c
Mathi2702 <elamathi.0409@gmail.com>
2023-04-19 06:03:37 -0600

    Added all the files

---
+++ static/contact.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['submit'])) {
+    $to = "elamathi.0409@gmail.com"; // Enter your email address here
+    $subject = $_POST['subject'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $headers = "From: ".$name." <".$email.">"."\r\n".
+               "Reply-To: ".$email."\r\n".
+               "X-Mailer: PHP/".phpversion();
+    mail($to, $subject, $message, $headers);
+    echo "<p>Thank you for contacting us, $name. We will get back to you soon.</p>";
+} else {
+    echo "<p>Sorry, there was an error with your submission. Please try again.</p>";
+}
+?>
